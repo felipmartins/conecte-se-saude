64ca2a585b4ce85adcff42eb6efee2ff96074e85
felipmartins <felipmartins94@gmail.com>
2023-10-02 18:46:56 -0300

    adiciona tailwind e tela de login

--- conectese/urls.py
+++ conectese/urls.py
@@ -1,4 +1,11 @@
+from conectese.views import index
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 
 
-urlpatterns = []
+
+urlpatterns = [
+    path('', index, name='home'),
+    path('login', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout', LogoutView.as_view(), name='logout')
+]
